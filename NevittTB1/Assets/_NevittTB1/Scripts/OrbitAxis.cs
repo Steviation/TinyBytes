e5e1c41aeb558babf6eaef681b57a0bebc283db8
Steviation <45446600+Steviation@users.noreply.github.com>
2022-09-08 14:55:29 -0600

    Update orbit script

--- NevittTB1/Assets/_NevittTB1/Scripts/OrbitAxis.cs
+++ NevittTB1/Assets/_NevittTB1/Scripts/OrbitAxis.cs
@@ -7,8 +7,6 @@ public class OrbitAxis : MonoBehaviour
 	public GameObject OrbitCenter;
 	public float rotationSpeed = 50.0f;
 	public float maxRadius = 10.0f;
-	private Vector3 center;
-	private Vector3 axis;
 
 	// Start is called before the first frame update
 	void Start()
@@ -19,13 +17,12 @@ public class OrbitAxis : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// Get orbit center position
-		center = OrbitCenter.transform.position;
-		axis = OrbitCenter.transform.up;
+		// Get orbit center orientation
+		Vector3 center = OrbitCenter.transform.position;
+		Vector3 axis = OrbitCenter.transform.up;
 		// Rotate around center point
 		transform.RotateAround(center, axis, rotationSpeed * Time.deltaTime);
 		// Clamp to maximum range from center
-		Vector3 offset = transform.position - center;
-		transform.position = center + Vector3.ClampMagnitude(offset, maxRadius);
+		transform.position = center + Vector3.ClampMagnitude(transform.position - center, maxRadius);
 	}
 }
